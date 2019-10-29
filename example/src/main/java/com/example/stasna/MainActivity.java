f44df8339ebb9d8fba4c3dd754b318f8b3c71d10
高树平 <1411198314@qq.com>
2019-10-29 18:47:23 +0800

    example示例更新

--- example/src/main/java/com/example/stasna/MainActivity.java
+++ example/src/main/java/com/example/stasna/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.stasna;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.satsna.compressimage.compress.CompressCallBack;
+import com.satsna.compressimage.compress.CompressHelper;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String coppress_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/1test_img";
+    String testImagePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/1test/test.jpg";
+    List<String> imgPathList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+    }
+
+    public void test() {
+        for (int i = 0; i < 20; i++) {
+            imgPathList.add(testImagePath);
+        }
+        CompressHelper.getDefault(imgPathList, new CompressCallBack() {
+            @Override
+            public void onFinish(List<File> compressList) {
+                Log.e("", "压缩完成");
+            }
+        });
+
     }
 }
